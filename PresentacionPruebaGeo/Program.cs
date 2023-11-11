dfd69b375795b4f74ede728535753131990af338
prado <pradojimenezmaria@gmail.com>
2023-11-10 18:32:28 -0600

    Agregar archivos de proyecto.

---
+++ PresentacionPruebaGeo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentacionPruebaGeo
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FrmPrincipal());
+        }
+    }
+}
